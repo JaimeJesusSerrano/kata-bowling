4262210b7db94b41eb620e94acd14d34eb861434
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2023-12-29 23:46:16 +0100

    feat: add isDoubleScore

--- src/tests/bowlingGame.test.ts
+++ src/tests/bowlingGame.test.ts
@@ -1,10 +1,31 @@
 // import { calculateBowlingScore } from '../core/bowlingGame';
 
 class BowlingGame {
+	TOTAL_BOWLS = 10;
 	rolls: number[] = [];
 
 	calculateTotalScore() {
-		return this.rolls.reduce((previous, current) => previous + current, 0);
+		const totalThrows = this.rolls.length;
+		let score = 0;
+		let isDoubleScore = false;
+
+		for (let i = 0; i < totalThrows; i++) {
+			const currentRoll = this.rolls[i];
+			const previousRoll = i === 0 ? undefined : this.rolls[i - 1];
+
+			if (isDoubleScore) {
+				score += currentRoll * 2;
+				isDoubleScore = false;
+			} else {
+				score += currentRoll;
+			}
+
+			if (currentRoll === this.TOTAL_BOWLS || (previousRoll && previousRoll + currentRoll === this.TOTAL_BOWLS)) {
+				isDoubleScore = true;
+			}
+		}
+
+		return score;
 	}
 
 	roll(pins: number) {
@@ -38,7 +59,16 @@ describe('The Bowling Game', () => {
 		expect(game.calculateTotalScore()).toBe(20);
 	});
 
+	it('calculate the score for a given spare and extra ball', () => {
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		rollMany(17, 0);
+		expect(game.calculateTotalScore()).toBe(20);
+	});
+
 	function rollMany(times: number, pins: number) {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		Array.from({ length: times }).forEach((_) => game.roll(pins));
 	}
 });
