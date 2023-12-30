8996a862413d7c832630c79882a18070df86d920
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2023-12-30 08:31:06 +0100

    feat: update game logic

--- src/core/bowlingGame.ts
+++ src/core/bowlingGame.ts
@@ -1,30 +1,30 @@
 export class BowlingGame {
-	TOTAL_BOWLS = 10;
-	DOUBLE_VALUE_TIMES_IF_STRIKE = 2;
-	DOUBLE_VALUE_TIMES_IF_SPARE = 1;
-	rolls: number[] = [];
+	private readonly MAX_SCORE_PER_FRAME = 10;
+
+	private rolls: number[] = [];
 
 	calculateTotalScore() {
-		const totalThrows = this.rolls.length;
+		const totalFrames = this.rolls.length;
 
-		let doubleValueTimes = 0;
 		let score = 0;
 
-		for (let i = 0; i < totalThrows; i++) {
+		for (let i = 0; i < totalFrames; i++) {
 			const currentRoll: number = this.rolls[i];
-			const nextRoll: number | undefined = this.rolls[i + 1];
 
-			score = this.getNewScore(score, currentRoll, doubleValueTimes > 0);
-			doubleValueTimes = this.decreaseDoubleValueTimes(doubleValueTimes);
-
-			const isSpare = this.isSpare(currentRoll, nextRoll);
-			if (isSpare) {
-				score = this.getNewScore(score, nextRoll, doubleValueTimes > 0);
-				doubleValueTimes = this.decreaseDoubleValueTimes(doubleValueTimes);
-				i++;
+			const isStrike = this.isStrike(i);
+			if (isStrike) {
+				score += this.getStrikeScore(i);
+				continue;
 			}
 
-			doubleValueTimes = this.getDoubleValueTimes(currentRoll, nextRoll, doubleValueTimes);
+			const isSpare = this.isSpare(i);
+			if (isSpare) {
+				score += this.getSpareScore(i);
+				i++;
+				continue;
+			}
+
+			score += currentRoll;
 		}
 
 		return score;
@@ -34,34 +34,24 @@ export class BowlingGame {
 		this.rolls.push(pins);
 	}
 
-	private getNewScore(score: number, newValue: number, isDoubleValue: boolean) {
-		return score + (isDoubleValue ? newValue * 2 : newValue);
+	private isSpare(frame: number) {
+		return this.rolls[frame] + this.rolls[frame + 1] === this.MAX_SCORE_PER_FRAME;
 	}
 
-	private decreaseDoubleValueTimes(doubleValueTimes: number) {
-		return doubleValueTimes ? doubleValueTimes - 1 : 0;
+	private isStrike(frame: number) {
+		return this.rolls[frame] === this.MAX_SCORE_PER_FRAME;
 	}
 
-	private getDoubleValueTimes(currentRoll: number, nextRoll: number, doubleValueTimes: number) {
-		const isStrike = this.isStrike(currentRoll);
-		if (isStrike) {
-			return this.DOUBLE_VALUE_TIMES_IF_STRIKE;
-		}
-
-		const isSpare = this.isSpare(currentRoll, nextRoll);
-		if (isSpare) {
-			return this.DOUBLE_VALUE_TIMES_IF_SPARE;
-		}
-
-		return doubleValueTimes;
+	private getSpareScore(frame: number) {
+		const nextFrame = this.rolls[frame + 1] || 0;
+		const nextNextFrame = this.rolls[frame + 2] || 0;
+		return this.rolls[frame] + nextFrame + nextNextFrame;
 	}
 
-	private isSpare(currentRoll: number, nextRoll: number) {
-		return currentRoll < this.TOTAL_BOWLS && nextRoll < this.TOTAL_BOWLS && currentRoll + nextRoll === this.TOTAL_BOWLS;
-	}
-
-	private isStrike(currentRoll: number) {
-		return currentRoll === this.TOTAL_BOWLS;
+	private getStrikeScore(frame: number) {
+		const nextFrame = this.rolls[frame + 1] || 0;
+		const nextNextFrame = this.rolls[frame + 2] || 0;
+		return this.rolls[frame] + nextFrame + nextNextFrame;
 	}
 }
 
